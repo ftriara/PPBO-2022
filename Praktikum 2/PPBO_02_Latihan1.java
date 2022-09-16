1a38f6766c2be99e487bbfc1a07a57bd7ff745cc
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-16 19:39:15 +0700

    Create PPBO_02_Latihan1.java

---
+++ Praktikum 2/PPBO_02_Latihan1.java
@@ -0,0 +1,25 @@
+// Nama : Fitria Rahmadani
+// NIM  : M0521022
+public class PPBO_02_Latihan1 {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 3.0;
+        double c = 4.3f;
+
+        double a1 = Double.valueOf(a);
+        double c1 = Double.valueOf(c);
+
+        double sum = a1 + b + c1;
+
+        System.out.printf("%.1f", sum);
+    }
+}
+
+// ubah variabel a dan c ke tipe data double
+// jumlahkan variabel a, b, dan c
+// tampilkan hasil penjumlahan ketiga variabel
+
+/*
+ * Output yang diharapkan:
+    17.3
+ */
